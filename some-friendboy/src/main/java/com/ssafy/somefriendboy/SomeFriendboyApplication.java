ea6e3229d1eb63d8907ad6c77ac8f9ebdc0a5b63
chahk03 <chku52@gmail.com>
2023-04-24 13:46:12 +0900

    [BE] add : friendboy project init

---
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/SomeFriendboyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.somefriendboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeFriendboyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeFriendboyApplication.class, args);
+	}
+
+}
