c3c941213279cfc3465f6a098f3c98d958f40440
박서윤 <seoyoon528@gmail.com>
2023-04-24 13:33:43 +0900

    [BE] add : project init

---
+++ some-auth/src/main/java/com/ssafy/someauth/SomeAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.someauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeAuthApplication.class, args);
+	}
+
+}
---
+++ some-auth/src/test/java/com/ssafy/someauth/SomeAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.someauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomeAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
