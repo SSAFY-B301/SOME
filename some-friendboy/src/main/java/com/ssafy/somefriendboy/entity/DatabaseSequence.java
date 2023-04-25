75e2d5028313d059c9407a1bde626ad55be6149d
차현경 <h30young@naver.com>
2023-04-25 14:20:10 +0900

    [BE] add : DatabaseSequence entity

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/entity/DatabaseSequence.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/entity/DatabaseSequence.java
@@ -1,2 +1,14 @@
-package com.ssafy.somefriendboy.entity;public class DatabaseSequence {
+package com.ssafy.somefriendboy.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Id;
+
+@Document
+public class DatabaseSequence {
+
+    @Id
+    private String id;
+    private Long seq;
+
 }
