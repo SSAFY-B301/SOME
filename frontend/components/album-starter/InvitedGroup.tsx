2cdc07df886c43619d42e4b925c6c511f1cbf864
euntol <mokbee27@gmail.com>
2023-04-25 12:42:44 +0900

    [FE] add: 친구 초대 페이지 틀

---
+++ frontend/components/album-starter/InvitedGroup.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import InvitedFriend from "./InvitedFriend";
+
+interface FriendType {
+  id: string;
+  profileImg: string;
+  name: string;
+}
+
+interface Props {
+  group: FriendType[];
+}
+
+const InvitedGroup = ({ group }: Props): JSX.Element => {
+  return (
+    <ul className="flex gap-4 overflow-scroll">
+      {group.map((friend) => (
+        <InvitedFriend key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+};
+
+export default InvitedGroup;
