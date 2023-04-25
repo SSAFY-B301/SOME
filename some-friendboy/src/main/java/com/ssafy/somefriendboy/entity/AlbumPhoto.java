dc66e9a51bcfc8f5fc6728bfb7c4c313408df42c
차현경 <h30young@naver.com>
2023-04-25 14:19:27 +0900

    [BE] add : AlbumPhoto entity

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/entity/AlbumPhoto.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/entity/AlbumPhoto.java
@@ -1,2 +1,25 @@
-package com.ssafy.somefriendboy.entity;public class AlbumPhoto {
+package com.ssafy.somefriendboy.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.joda.time.LocalDateTime;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Document
+@Getter @Setter
+public class AlbumPhoto {
+
+    @Id
+    private Long photoId;
+    private LocalDateTime uploadedDate;
+    private LocalDateTime shootDate;
+    private String s3Url;
+    private String shootLocation;
+    private String category;
+    private Long userId;
+    private Album album;
+
 }
