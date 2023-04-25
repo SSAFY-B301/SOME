aa7da8b2907e94447c0530d44346141bcc110205
차현경 <h30young@naver.com>
2023-04-25 14:17:34 +0900

    [BE] add : Album entity

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/entity/Album.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/entity/Album.java
@@ -1,2 +1,28 @@
-package com.ssafy.somefriendboy.entity;public class Album {
+package com.ssafy.somefriendboy.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.joda.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Album {
+
+    @Id
+    @GeneratedValue
+    private Long albumId;
+    private String albumName;
+    private LocalDateTime createdDate;
+    private Long recentPhoto;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String type;
+    private String status;
+
 }
