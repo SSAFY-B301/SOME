13905b9705dba0d541185160a989c98cb3c8a6a2
정상민 <jsangmin4175@gmail.com>
2023-04-25 13:31:07 +0900

    [BE] modify: 카테고리 배열, pip install
    Former-commit-id: 3f4bbda70d80e9dafddc39f75e476ef6423ea4df

--- fast.py
+++ fast.py
@@ -78,7 +78,7 @@ def yololo():
     return { 'ids' : category}
 
 @app.get('/yolo/file')
-async def yololo(file: UploadFile = File(...)):
+async def yololo(file: list[UploadFile] = File(...)):
     min_confidence = 0.5
 
     # Load Yolo
@@ -91,57 +91,60 @@ async def yololo(file: UploadFile = File(...)):
     output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
 
     colors = np.random.uniform(0, 255, size=(len(classes), 3))
-
-    file_contents = await file.read()
-    np_array = np.frombuffer(file_contents, np.uint8)
-    # Loading image
-    img = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
-    #img = cv2.imread("image/yolo_01.jpg")
-    #img = cv2.resize(img, None, fx=0.4, fy=0.4)
-    height, width, channels = img.shape
-
-    # Detecting objects
-    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-
-    net.setInput(blob)
-    outs = net.forward(output_layers)
-
-    # Showing informations on the screen
-    class_ids = []
-    confidences = []
-    boxes = []
     category = []
-    for out in outs:
-        for detection in out:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > min_confidence:
-                # Object detected
-                center_x = int(detection[0] * width)
-                center_y = int(detection[1] * height)
-                w = int(detection[2] * width)
-                h = int(detection[3] * height)
 
-                # Rectangle coordinates
-                x = int(center_x - w / 2)
-                y = int(center_y - h / 2)
+    for f in file:
+        file_contents = await f.read()
+        np_array = np.frombuffer(file_contents, np.uint8)
+        # Loading image
+        img = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+        #img = cv2.imread("image/yolo_01.jpg")
+        #img = cv2.resize(img, None, fx=0.4, fy=0.4)
+        height, width, channels = img.shape
 
-                boxes.append([x, y, w, h])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
-                
+        # Detecting objects
+        blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
 
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, min_confidence, 0.4)
-    font = cv2.FONT_HERSHEY_PLAIN
-    for i in range(len(boxes)):
-        if i in indexes:
-            x, y, w, h = boxes[i]
-            label = str(classes[class_ids[i]])
-            category.append(str(classes[class_ids[i]]))
-            print(i, label)
-            color = colors[i]
-            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-            cv2.putText(img, label, (x, y + 30), font, 2, (0, 255, 0), 1)
+        net.setInput(blob)
+        outs = net.forward(output_layers)
 
-    return { 'ids' : category}
+        # Showing informations on the screen
+        class_ids = []
+        confidences = []
+        boxes = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > min_confidence:
+                    # Object detected
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+
+                    # Rectangle coordinates
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+
+                    boxes.append([x, y, w, h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+                    
+
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, min_confidence, 0.4)
+        font = cv2.FONT_HERSHEY_PLAIN
+        categoryTemp = []
+        for i in range(len(boxes)):
+            if i in indexes:
+                x, y, w, h = boxes[i]
+                label = str(classes[class_ids[i]])
+                categoryTemp.append(str(classes[class_ids[i]]))
+                print(i, label)
+                color = colors[i]
+                cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+                cv2.putText(img, label, (x, y + 30), font, 2, (0, 255, 0), 1)
+        category.append(categoryTemp)
+
+    return { 'category' : category}
