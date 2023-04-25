5a5291f8ccde5c9522c70f177b3dec850e56ae46
정상민 <jsangmin4175@gmail.com>
2023-04-25 12:39:24 +0900

    [BE] add: fastAPI, YOLO 테스트 완료

--- fast.py
+++ fast.py
@@ -1,53 +1,147 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, File, UploadFile
+import cv2
+import numpy as np
 
 app = FastAPI()
 
-db = []
-class Paste(BaseModel):
-    content: str
 
-
-@app.get('/')
+@app.get("/")
 def root():
     return {'message': 'Hello World'}
 
 
-@app.get('/paste/{paste_id}')
-def get_paste(paste_id: int):
-    if paste_id < len(db):
-        return {'paste_id': paste_id,
-                'paste': db[paste_id]}
-    else:
-        return {'paste_id': paste_id,
-                'paste': None}
 
+@app.get('/yolo')
+def yololo():
+    min_confidence = 0.5
 
-@app.post('/paste/')
-def post_paste(paste: Paste):
-    db.append(paste)
-    paste_id = len(db)-1
-    return {'paste_id': paste_id,
-            'paste': db[paste_id]}
+    # Load Yolo
+    net = cv2.dnn.readNet("yolo/yolov3.weights", "yolo/yolov3.cfg")
+    classes = []
+    with open("yolo/coco.names", "r") as f:
+        classes = [line.strip() for line in f.readlines()]
+    layer_names = net.getLayerNames()
+    #output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
 
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
-@app.put('/paste/{paste_id}')
-def put_paste(paste_id: int, paste: Paste):
-    if paste_id < len(db):
-        db[paste_id] = paste
-        return {'paste_id': paste_id, 
-                'paste': paste}
-    else:
-        return {'paste_id': paste_id,
-                'paste': None}
+    # Loading image
+    img = cv2.imread("image/yolo_01.jpg")
+    #img = cv2.resize(img, None, fx=0.4, fy=0.4)
+    height, width, channels = img.shape
+
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # Showing informations on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+    category = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > min_confidence:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
                 
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, min_confidence, 0.4)
+    font = cv2.FONT_HERSHEY_PLAIN
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            category.append(str(classes[class_ids[i]]))
+            print(i, label)
+            color = colors[i]
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(img, label, (x, y + 30), font, 2, (0, 255, 0), 1)
+
+    return { 'ids' : category}
+
+@app.get('/yolo/file')
+async def yololo(file: UploadFile = File(...)):
+    min_confidence = 0.5
+
+    # Load Yolo
+    net = cv2.dnn.readNet("yolo/yolov3.weights", "yolo/yolov3.cfg")
+    classes = []
+    with open("yolo/coco.names", "r") as f:
+        classes = [line.strip() for line in f.readlines()]
+    layer_names = net.getLayerNames()
+    #output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+    file_contents = await file.read()
+    np_array = np.frombuffer(file_contents, np.uint8)
+    # Loading image
+    img = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+    #img = cv2.imread("image/yolo_01.jpg")
+    #img = cv2.resize(img, None, fx=0.4, fy=0.4)
+    height, width, channels = img.shape
+
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # Showing informations on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+    category = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > min_confidence:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
                 
-@app.delete('/paste/{paste_id}')
-def delete_paste(paste_id:int):
-    if paste_id < len(db):
-        db[paste_id] = None
-        return {'paste_id': paste_id,
-                'paste': db[paste_id]}
-    else:
-        return {'paste_id': paste_id,
-                'paste': None}
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, min_confidence, 0.4)
+    font = cv2.FONT_HERSHEY_PLAIN
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            category.append(str(classes[class_ids[i]]))
+            print(i, label)
+            color = colors[i]
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(img, label, (x, y + 30), font, 2, (0, 255, 0), 1)
+
+    return { 'ids' : category}
