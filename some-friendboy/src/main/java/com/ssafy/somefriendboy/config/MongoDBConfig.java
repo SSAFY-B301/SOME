b71b5c0d1d21fadeae5647199cdc47c302e55cc7
차현경 <h30young@naver.com>
2023-04-25 14:20:36 +0900

    [BE] add : MongoDB config

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/config/MongoDBConfig.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/config/MongoDBConfig.java
@@ -1,6 +1,5 @@
-package com.ssafy.sometest.config;
+package com.ssafy.somefriendboy.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
