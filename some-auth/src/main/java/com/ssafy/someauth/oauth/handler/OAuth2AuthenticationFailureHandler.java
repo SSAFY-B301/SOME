3e1c9207414680a45fbe231a2dd9dae9eb84473c
박서윤 <seoyoon528@gmail.com>
2023-04-25 17:48:25 +0900

    [BE] add : OAuth Authorization

---
+++ some-auth/src/main/java/com/ssafy/someauth/oauth/handler/OAuth2AuthenticationFailureHandler.java
@@ -0,0 +1,41 @@
+package com.ssafy.someauth.oauth.handler;
+
+import com.ssafy.someauth.oauth.repository.OAuth2AuthorizationRequestBasedOnCookieRepository;
+import com.ssafy.someauth.util.CookieUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.ssafy.someauth.oauth.repository.OAuth2AuthorizationRequestBasedOnCookieRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
+
+/* 소셜 인증에 실패 했을때 Handler */
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    private final OAuth2AuthorizationRequestBasedOnCookieRepository authorizationRequestRepository;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        String targetUrl = CookieUtil.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+                .map(Cookie::getValue)
+                .orElse(("/"));
+
+        exception.printStackTrace();
+        targetUrl = UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam("error", exception.getLocalizedMessage())
+                .build().toUriString();
+
+        authorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+}
