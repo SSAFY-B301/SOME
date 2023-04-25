1acca4ee5e58a8bac1837737c3da74e986c6370d
oth54477 <oth5447@naver.com>
2023-04-25 11:12:22 +0900

    [FE] fix : 컴포넌트 분리 및 검색 바 수정

--- frontend/components/common/TotalAlbum.tsx
+++ frontend/components/common/TotalAlbum.tsx
@@ -1,60 +1,28 @@
-import styles from "@/styles/home.module.scss";
-import { useState } from "react";
-import { totalAlbumsApi } from "@/pages/api/dummyapi";
+import styles from "@/styles/search.module.scss";
+import { useEffect, useState } from "react";
+import { totalAlbumsApi } from "@/pages/api/totalDummyApi";
 import SearchIcon from "@/public/icons/MagnifyingGlass.svg";
 import HeartIcon from "@/public/icons/Heart.svg";
 import { useRouter } from "next/router";
 
 function TotalAlbum() {
-  const [flag, setFlag] = useState<boolean>(false);
-  const [isClicked, setIsClicked] = useState<boolean>(false);
-  const [touchPosition, setTouchPosition] = useState<number>();
-  const [touchStart, setTouchStart] = useState<number>(0);
-  const [isSearch, setIsSearch] = useState<boolean>(false);
+  const [isTotal, setIsTotal] = useState<boolean>(false);
+  const [isMove, setIsMove] = useState<boolean>(false);
+  const [touchPosition, setTouchPosition] = useState<number>(0);
 
   return (
     <section
       style={{ top: `${touchPosition}` + "px" }}
       className={`${styles.total_album} ${
-        !flag && (isClicked ? styles.click_total_album : styles.no_click)
+        !isMove && (isTotal ? styles.click_total_album : styles.no_click)
       } bg-white dark:bg-dark-block`}
     >
-      <div
-        onClick={() => {
-          setIsClicked(true);
-        }}
-        onTouchStart={(e) => {
-          setTouchStart(e.changedTouches[0].pageY);
-        }}
-        onTouchMove={(e) => {
-          setTouchPosition(e.changedTouches[0].pageY);
-          setFlag(true);
-        }}
-        onTouchEnd={(e) => {
-          setFlag(false);
-          touchStart - e.changedTouches[0].pageY > 0
-            ? setIsClicked(true)
-            : setIsClicked(false);
-        }}
-        className="flex justify-between items-center w-full"
-        style={{ padding: "4.103vw" }}
-      >
-        <div
-          className={`font-bold ${
-            isSearch ? styles.search : styles.no_search
-          } ${styles.search_box}`}
-          style={{ fontSize: "5.128vw" }}
-        >
-          <div></div>
-          <span>전체 앨범</span>
-        </div>
-        <SearchIcon
-          onClick={() => {
-            setIsSearch(!isSearch);
-          }}
-          stroke="black"
-        />
-      </div>
+      <Header
+        setIsTotal={setIsTotal}
+        setIsMove={setIsMove}
+        setTouchPosition={setTouchPosition}
+        isTotal={isTotal}
+      />
       <TotalAlbumItems />
     </section>
   );
@@ -68,6 +36,10 @@ interface TotalAlbumType {
   isLike: boolean;
 }
 
+/**
+ * 전체 앨범 리스트
+ * @returns
+ */
 function TotalAlbumItems() {
   const router = useRouter();
   const [albums, setAlbums] = useState<TotalAlbumType[]>(totalAlbumsApi.data);
@@ -105,6 +77,72 @@ function TotalAlbumItems() {
         {totalAlbums}
       </div>
     </section>
+  );
+}
+
+interface HeaderType {
+  setIsTotal: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsMove: React.Dispatch<React.SetStateAction<boolean>>;
+  setTouchPosition: React.Dispatch<React.SetStateAction<number>>;
+  isTotal: boolean;
+}
+
+/**
+ * 전체 앨범 검색 부분
+ * @param props
+ * @returns
+ */
+function Header({
+  setIsTotal,
+  setIsMove,
+  setTouchPosition,
+  isTotal,
+}: HeaderType) {
+  const [touchStart, setTouchStart] = useState<number>(0);
+  const [isSearch, setIsSearch] = useState<boolean>(false);
+  const [flag, setFlag] = useState<boolean>(false);
+  useEffect(() => {
+    !isTotal && setIsSearch(false);
+  }, [isTotal]);
+
+  return (
+    <div
+      onClick={() => {
+        setIsTotal(true);
+      }}
+      onTouchStart={(e) => {
+        setTouchStart(e.changedTouches[0].pageY);
+      }}
+      onTouchMove={(e) => {
+        setTouchPosition(e.changedTouches[0].pageY);
+        setIsMove(true);
+      }}
+      onTouchEnd={(e) => {
+        setIsMove(false);
+        touchStart - e.changedTouches[0].pageY >= 0
+          ? setIsTotal(true)
+          : setIsTotal(false);
+      }}
+      className="flex justify-between items-center w-full"
+      style={{ padding: "4.103vw" }}
+    >
+      <div
+        className={`font-bold ${
+          flag && (isSearch ? styles.search : styles.no_search)
+        } ${styles.search_box}`}
+        style={{ fontSize: "5.128vw" }}
+      >
+        <div>{isSearch && <input type="text" />}</div>
+        <span>전체 앨범</span>
+      </div>
+      <SearchIcon
+        onClick={() => {
+          isTotal && setFlag(true);
+          setIsSearch(!isSearch);
+        }}
+        stroke="black"
+      />
+    </div>
   );
 }
 
