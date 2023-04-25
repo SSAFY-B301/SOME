e81cab20694139c788bc0a2f411826a1d092721f
oth54477 <oth5447@naver.com>
2023-04-25 11:21:19 +0900

    [FE] fix : 스타일 경로 변경

--- frontend/components/common/TotalAlbum.tsx
+++ frontend/components/common/TotalAlbum.tsx
@@ -1,4 +1,4 @@
-import styles from "@/styles/search.module.scss";
+import styles from "@/styles/total.module.scss";
 import { useEffect, useState } from "react";
 import { totalAlbumsApi } from "@/pages/api/totalDummyApi";
 import SearchIcon from "@/public/icons/MagnifyingGlass.svg";
