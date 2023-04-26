339c2ebae1c037e23cb19b34bbfbd938c798ca73
차현경 <h30young@naver.com>
2023-04-26 09:55:24 +0900

    [BE] : fix AlbumPhoto entity

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/entity/AlbumPhoto.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/entity/AlbumPhoto.java
@@ -5,12 +5,15 @@ import lombok.Setter;
 import org.joda.time.LocalDateTime;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.Id;
-import javax.persistence.Transient;
+import javax.persistence.*;
 
 @Document
 @Getter @Setter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AlbumPhoto {
+
+    @Transient
+    public static final String SEQUENCE_NAME = "albumPhoto_sequence";
 
     @Id
     private Long photoId;
