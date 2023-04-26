ee8238bb4a714ee5ae1f0a1ed678583720b2c8f7
차현경 <h30young@naver.com>
2023-04-26 09:53:04 +0900

    [BE] : fix application.yml

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/entity/AutoIncrementSequence.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/entity/AutoIncrementSequence.java
@@ -1,11 +1,13 @@
 package com.ssafy.somefriendboy.entity;
 
+import lombok.Getter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Id;
 
 @Document
-public class DatabaseSequence {
+@Getter
+public class AutoIncrementSequence {
 
     @Id
     private String id;
