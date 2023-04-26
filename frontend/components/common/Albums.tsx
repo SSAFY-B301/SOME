34300be8aa16fb3d19b7a812bc22ac39edc0bdeb
oth54477 <oth5447@naver.com>
2023-04-26 11:17:55 +0900

    [FE] fix : 홈 더미 데이터 파일명 수정

--- frontend/components/common/Albums.tsx
+++ frontend/components/common/Albums.tsx
@@ -1,7 +1,5 @@
-// "use client";
-
 import { useState } from "react";
-import { currentAlbumsApi, favoriteAlbumsApi } from "@/pages/api/dummyApi";
+import { currentAlbumsApi, favoriteAlbumsApi } from "@/pages/api/homeDummyApi";
 import styles from "@/styles/home.module.scss";
 import HeartIcon from "@/public/icons/Heart.svg";
 import DotsIcon from "@/public/icons/DotsThreeOutline.svg";
