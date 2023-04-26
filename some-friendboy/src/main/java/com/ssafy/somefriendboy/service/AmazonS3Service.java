4642ef1f53bf985d866bf3af554819f852d3823d
차현경 <h30young@naver.com>
2023-04-26 09:59:42 +0900

    [BE] : add S3 service

---
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/service/AmazonS3Service.java
@@ -0,0 +1,39 @@
+package com.ssafy.somefriendboy.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class AmazonS3Service {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String S3Bucket;
+    private final AmazonS3Client amazonS3Client;
+
+    public String uploadFile(MultipartFile multipartFile) throws IOException {
+        String originalName = multipartFile.getOriginalFilename();
+        long size = multipartFile.getSize();
+
+        ObjectMetadata objectMetaData = new ObjectMetadata();
+        objectMetaData.setContentType(multipartFile.getContentType());
+        objectMetaData.setContentLength(size);
+
+        amazonS3Client.putObject(
+                new PutObjectRequest(S3Bucket, originalName, multipartFile.getInputStream(), objectMetaData)
+                        .withCannedAcl(CannedAccessControlList.PublicRead)
+        );
+
+        String imagePath = amazonS3Client.getUrl(S3Bucket, originalName).toString();
+        return imagePath;
+    }
+
+}
