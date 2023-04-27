31d0dbe98164f95114369362e6e795e0ba31cf8a
HyeonIn <gusdlsdlek@gmail.com>
2023-04-27 15:12:23 +0900

    [FE] add : Token log 추가

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -10,12 +10,15 @@ import { useRouter } from "next/router";
 
 export default function Home() {
   //로그인 상태인지 확인하고, 로그인 안 되어 있으면 로그인 페이지로 이동
-  const {isLogin} = useSelector((state:RootState) => state.auth);
+  const {isLogin, accessToken} = useSelector((state:RootState) => state.auth);
   const router = useRouter();
   
   useEffect(() => {
     if (!isLogin) {
       router.push("login")
+    }
+    else{
+      console.log("Token : " + accessToken)
     }
     return () => {
     }
