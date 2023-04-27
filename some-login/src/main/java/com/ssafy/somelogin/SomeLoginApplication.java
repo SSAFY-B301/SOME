9c8583d78493d238a65748c588f29299a36538ef
정상민 <jsangmin4175@gmail.com>
2023-04-28 00:10:28 +0900

    [BE] add: jpaqueryfactory 추가

--- some-login/src/main/java/com/ssafy/somelogin/SomeLoginApplication.java
+++ some-login/src/main/java/com/ssafy/somelogin/SomeLoginApplication.java
@@ -1,7 +1,11 @@
 package com.ssafy.somelogin;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class SomeLoginApplication {
@@ -9,5 +13,8 @@ public class SomeLoginApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SomeLoginApplication.class, args);
 	}
-
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em){
+		return new JPAQueryFactory(em);
+	}
 }
