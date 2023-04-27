8655790a3be7c1b98af3ac8944b88d2bd9146876
정상민 <jsangmin4175@gmail.com>
2023-04-28 00:25:42 +0900

    [BE] modify: config 수정

---
+++ some-login/src/main/java/com/ssafy/somelogin/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.ssafy.somelogin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").
+        allowedOrigins("k8b301.p.ssafy.io", "http://localhost:9001", "http://localhost:9002", "http://localhost:3000")
+                .exposedHeaders("authorization_code")
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+}
