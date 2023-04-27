a64ae1bc61464d1e53217566a49da30bbd836d38
차현경 <h30young@naver.com>
2023-04-27 12:58:59 +0900

    [BE] : fix input, output type

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/service/AmazonS3Service.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/service/AmazonS3Service.java
@@ -4,12 +4,15 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.ssafy.somefriendboy.dto.MetaDataDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,21 +22,29 @@ public class AmazonS3Service {
     private String S3Bucket;
     private final AmazonS3Client amazonS3Client;
 
-    public String uploadFile(MultipartFile multipartFile) throws IOException {
-        String originalName = multipartFile.getOriginalFilename();
-        long size = multipartFile.getSize();
+    public List<MetaDataDto> uploadFile(List<MultipartFile> multipartFiles) throws IOException {
+        List<MetaDataDto> metaDataDtos = new ArrayList<>();
 
-        ObjectMetadata objectMetaData = new ObjectMetadata();
-        objectMetaData.setContentType(multipartFile.getContentType());
-        objectMetaData.setContentLength(size);
+        for (MultipartFile multipartFile : multipartFiles) {
+            String originalName = multipartFile.getOriginalFilename();
+            long size = multipartFile.getSize();
 
-        amazonS3Client.putObject(
-                new PutObjectRequest(S3Bucket, originalName, multipartFile.getInputStream(), objectMetaData)
-                        .withCannedAcl(CannedAccessControlList.PublicRead)
-        );
+            ObjectMetadata objectMetaData = new ObjectMetadata();
+            objectMetaData.setContentType(multipartFile.getContentType());
+            objectMetaData.setContentLength(size);
 
-        String imagePath = amazonS3Client.getUrl(S3Bucket, originalName).toString();
-        return imagePath;
+            amazonS3Client.putObject(
+                    new PutObjectRequest(S3Bucket, originalName, multipartFile.getInputStream(), objectMetaData)
+                            .withCannedAcl(CannedAccessControlList.PublicRead)
+            );
+
+            String imagePath = amazonS3Client.getUrl(S3Bucket, originalName).toString();
+            MetaDataDto metaDataDto = MetaDataDto.builder().multipartFile(multipartFile)
+                    .url(imagePath).build();
+            metaDataDtos.add(metaDataDto);
+        }
+
+        return metaDataDtos;
     }
 
 }
