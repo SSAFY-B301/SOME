3465ee32d44a04170c6b7448b47a124550a8c13b
HyeonIn <gusdlsdlek@gmail.com>
2023-04-27 12:27:30 +0900

    [FE] add : 로그인 페이지 슬라이더 추가

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import wrapper from "@/store";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App({ Component, pageProps }: AppProps) {
   return (
