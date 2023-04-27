e976604ae27c32ebe2f50da19dada206819e50d7
HyeonIn <gusdlsdlek@gmail.com>
2023-04-27 16:50:39 +0900

    [FE] refactor : 미사용 라이브러리 import 수정

--- frontend/components/pages/profile/ToggleList.tsx
+++ frontend/components/pages/profile/ToggleList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 export default function ToggleList() {
     const [toggleSns, setToggleSns] = useState<boolean>(false);
