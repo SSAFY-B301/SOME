40e6202d5e695632d836f82a4a3afff760d94d77
HyeonIn <gusdlsdlek@gmail.com>
2023-04-28 17:27:13 +0900

    [FE] fix : 알림 페이지 다크모드 추가

--- frontend/pages/api/notiDummyApi.tsx
+++ frontend/pages/api/notiDummyApi.tsx
@@ -1,7 +1,6 @@
 // Notification 타입 정의
 // 프로퍼티 설명
 // status : 알림 종류
-//      - 0 : 새로운 사진 등록
 //      - 1 : SNS 올리기 요청
 //      - 2 : 앨범 초대 알림
 interface NotificationType {
@@ -10,7 +9,6 @@ interface NotificationType {
     albumName : string,
     userId : string,
     nickname : string,
-    albumImg : string,
     notiImg? : string,
     status : number,
     isRead : boolean,
@@ -23,7 +21,6 @@ const notiDummy : NotificationType[] = [
         albumName : "FFF 앨범",
         userId : "0006",
         nickname : "김태영",
-        albumImg : "https://placeimg.com/48/48/any",
         status : 2,
         isRead : false,
     },
@@ -33,7 +30,6 @@ const notiDummy : NotificationType[] = [
         albumName : "EEE 앨범",
         userId : "0004",
         nickname : "박서윤",
-        albumImg : "https://placeimg.com/48/48/any",
         status : 2,
         isRead : false,
     },
@@ -43,7 +39,6 @@ const notiDummy : NotificationType[] = [
         albumName : "DDD 앨범",
         userId : "0003",
         nickname : "차현경",
-        albumImg : "https://placeimg.com/48/48/any",
         status : 2,
         isRead : false,
     },
@@ -53,31 +48,8 @@ const notiDummy : NotificationType[] = [
         albumName : "CCC 앨범",
         userId : "0002",
         nickname : "김동욱",
-        albumImg : "https://placeimg.com/48/48/any",
-        notiImg : "https://placeimg.com/48/48/any",
+        notiImg : "https://images.unsplash.com/photo-1555169062-013468b47731?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8JUVCJThGJTk5JUVCJUFDJUJDfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
         status : 1,
-        isRead : true,
-    },
-    {
-        notificationId : 1,
-        albumId : 2,
-        albumName : "BBB 앨범",
-        userId : "0001",
-        nickname : "정상민",
-        albumImg : "https://placeimg.com/48/48/any",
-        notiImg : "https://placeimg.com/48/48/any",
-        status : 0,
-        isRead : true,
-    },
-    {
-        notificationId : 0,
-        albumId : 1,
-        albumName : "AAA 앨범",
-        userId : "0000",
-        nickname : "최현인",
-        albumImg : "https://placeimg.com/48/48/any",
-        notiImg : "https://placeimg.com/48/48/any",
-        status : 0,
         isRead : true,
     },
 ]
--- frontend/pages/notification.tsx
+++ frontend/pages/notification.tsx
@@ -1,35 +1,28 @@
-import Link from "next/link";
-import CaretLeft from "public/icons/CaretLeft.svg";
 import { notiDummy } from "./api/notiDummyApi";
 import styles from "@/styles/notification.module.scss"
 import { InfoBar } from "@/components/common/Nav";
 
 export default function Notification() {
     return(
-        <div className="flex flex-col justify-start h-screen bg-white">
+        <div className="flex flex-col justify-start h-screen bg-white dark:bg-dark-bg-home">
             <InfoBar title="알림"></InfoBar>
             <div className="notiList">
                 {
                     notiDummy.map((noti) => {
                         return(
-                            <div key={noti.notificationId} className={noti.isRead ? `p-4 border-b-2` : `p-4 border-b-2 ${styles.on_read}`}>
+                            <div key={noti.notificationId} className={noti.isRead ? `py-4 px-6` : `py-4 px-6 ${styles.on_read}`}>
                                 <div className="flex justify-between">
                                     <div className="flex">
-                                        <div className="flex items-center">
-                                            {noti.status !== 2 && <img className="w-4 h-4 mr-2 rounded-sm " src={noti.albumImg} alt="" />}
-                                        </div>
-                                        <p className="text-gray-400">{noti.albumName}</p>
+                                        <p className="text-sm text-gray-400">{noti.albumName}</p>
                                     </div>
                                     <p className="text-sm text-gray-400">시간</p>
                                 </div>
-                                <div className={noti.isRead ? "flex justify-between mt-2 text-gray-400" : "flex justify-between mt-2"}>
-                                    <div>
-                                        {noti.status === 0 && <p>{noti.nickname}님이 새로운 사진을 등록했습니다!</p>}
-                                        {noti.status === 1 && <p>{noti.nickname}님이 SNS에 올리기를 요청했습니다!</p>}
-                                        {noti.status === 2 && <p>{noti.nickname}님이 새로운 앨범에 초대했습니다!</p>}
+                                <div className={noti.isRead ? "flex justify-between items-center mt-2 text-gray-400" : "mt-2 flex justify-between"}>
+                                    <div className="flex items-center justify-center h-12">
+                                        {noti.status === 1 && <p className="text-sm">{noti.nickname}님이 SNS에 올리기를 요청했습니다!</p>}
+                                        {noti.status === 2 && <p className="text-sm">{noti.nickname}님이 새로운 앨범에 초대했습니다!</p>}
                                     </div>
-                                    {noti.status !== 2 && <img src={noti.notiImg} alt="" />}
-                                    {noti.status === 2 && <img src={noti.albumImg} alt="" />}
+                                    {noti.status !== 2 && <img className="w-12 h-12 rounded-md" src={noti.notiImg} alt="" />}
                                 </div>
                             </div>
                         )
