097f54e2b82d22543845afa67261d7c4dad3296f
정상민 <jsangmin4175@gmail.com>
2023-04-28 14:37:58 +0900

    [BE] modify: 카카오친구목록 수정

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/dto/KakaoFriendResponseDto.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/dto/KakaoFriendResponseDto.java
@@ -1,14 +1,19 @@
 package com.ssafy.somefriendboy.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class KakaoFriendResponseDto {
+    @JsonProperty("after_url")
     private String afterUrl;
     private List<Friend> elements;
+
+    @JsonProperty("total_count")
     private int totalCount;
+    @JsonProperty("favorite_count")
     private int favoriteCount;
 
     @Data
@@ -16,7 +21,9 @@ public class KakaoFriendResponseDto {
         private long id;
         private String uuid;
         private boolean favorite;
+        @JsonProperty("profile_nickname")
         private String profileNickname;
+        @JsonProperty("profile_thumbnail_image")
         private String profileThumbnailImage;
     }
 }
