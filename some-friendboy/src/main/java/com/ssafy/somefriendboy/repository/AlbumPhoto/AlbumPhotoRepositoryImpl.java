dfd002585393ea85005333865351a36c89b434b0
차현경 <h30young@naver.com>
2023-04-28 18:13:29 +0900

    [BE] add : findAlbumCategoryPhoto method

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/repository/AlbumPhoto/AlbumPhotoRepositoryImpl.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/repository/AlbumPhoto/AlbumPhotoRepositoryImpl.java
@@ -1,4 +1,34 @@
 package com.ssafy.somefriendboy.repository.AlbumPhoto;
 
-public class AlbumPhotoRepositoryImpl {
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.somefriendboy.entity.AlbumPhoto;
+import com.ssafy.somefriendboy.util.MongoQueryUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
+import static com.ssafy.somefriendboy.entity.QAlbumPhoto.albumPhoto;
+
+@RequiredArgsConstructor
+public class AlbumPhotoRepositoryImpl implements AlbumPhotoRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+    private final MongoTemplate mongoTemplate;
+
+    @Override
+    public List<AlbumPhoto> findAlbumCategoryPhoto(Long albumId, Long categoryId) {
+        Query query = new Query();
+        query.addCriteria(new Criteria().andOperator(Criteria.where(MongoQueryUtil.parse(albumPhoto.albumId)).is(albumId),
+                new Criteria().orOperator(Criteria.where(MongoQueryUtil.parse(albumPhoto.categoryId)).in(categoryId))));
+        return mongoTemplate.find(query, AlbumPhoto.class);
+    }
+
+    private BooleanExpression categoryIdEq(Long categoryId) {
+        return categoryId == null ? null : albumPhoto.categoryId.contains(categoryId);
+    }
+
 }
