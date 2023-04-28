1c0453728d672e12495583d16067dd06a5e447f2
정상민 <jsangmin4175@gmail.com>
2023-04-28 13:27:51 +0900

    [BE] add: http response code 수정

--- some-login/src/main/java/com/ssafy/somelogin/controller/AuthController.java
+++ some-login/src/main/java/com/ssafy/somelogin/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.ssafy.somelogin.util.HttpUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -24,7 +25,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/kakao")
-    public ResponseEntity<?> getKaKaoToken(@RequestHeader HttpHeaders headers) throws IOException {
+    public ResponseEntity<ResponseDto> getKaKaoToken(@RequestHeader HttpHeaders headers) throws IOException {
         log.info(headers.toString());
         if(headers.isEmpty()){
             log.info("비어있으면 뜹니다.");
@@ -35,14 +36,17 @@ public class AuthController {
         redirect_base = redirect_base.replace("]","");
         log.info("인가코드로 토큰 요청 POST: /member/kakao, code : {}, redirect : {}",authorization_code,redirect_base);
         ResponseDto responseDto = authService.getTokenAnduserInfo(authorization_code,redirect_base);
-        return ResponseEntity.ok().body(responseDto);
+        return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.OK);
     }
 
     @PostMapping("/userid")
-    public ResponseEntity<?> getUserId(@RequestHeader HttpHeaders headers) throws IOException {
+    public ResponseEntity<ResponseDto> getUserId(@RequestHeader HttpHeaders headers) throws IOException {
         String access_token = headers.get("access_token").toString();
         log.info("토큰 검증, 유저아이디 리턴 POST: /member/userid, access_token : {}",access_token);
         ResponseDto responseDto = authService.getUserId(access_token);
-        return ResponseEntity.ok().body(responseDto);
+        if(responseDto.getStatus_code() == 400){
+            return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.OK);
     }
 }
