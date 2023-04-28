505aa8aac8b4026e10f26f70d18f33f8ad1a85c6
차현경 <h30young@naver.com>
2023-04-28 18:13:29 +0900

    [BE] add : findAlbumCategoryPhoto method

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/repository/AlbumPhoto/AlbumPhotoRepository.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/repository/AlbumPhoto/AlbumPhotoRepository.java
@@ -3,6 +3,10 @@ package com.ssafy.somefriendboy.repository.AlbumPhoto;
 import com.ssafy.somefriendboy.entity.AlbumPhoto;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface AlbumPhotoRepository extends MongoRepository<AlbumPhoto, String>, AlbumPhotoRepositoryCustom {
     AlbumPhoto findByPhotoId(Long photoId);
+    List<AlbumPhoto> findByAlbumId(Long albumId);
+    List<AlbumPhoto> findByAlbumIdAndCategoryId(Long albumId, Long categoryId);
 }
--- some-friendboy/src/main/java/com/ssafy/somefriendboy/repository/AlbumPhoto/AlbumPhotoRepositoryCustom.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/repository/AlbumPhoto/AlbumPhotoRepositoryCustom.java
@@ -1,4 +1,9 @@
 package com.ssafy.somefriendboy.repository.AlbumPhoto;
 
+import com.ssafy.somefriendboy.entity.AlbumPhoto;
+
+import java.util.List;
+
 public interface AlbumPhotoRepositoryCustom {
+    List<AlbumPhoto> findAlbumCategoryPhoto(Long albumId, Long categoryId);
 }
