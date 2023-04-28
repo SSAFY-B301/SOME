6a9e510171cff8a4180b5fda18c788d2f324b242
정상민 <jsangmin4175@gmail.com>
2023-04-28 11:13:07 +0900

    [BE] modify: 로그 수정

--- some-login/src/main/java/com/ssafy/somelogin/controller/AuthController.java
+++ some-login/src/main/java/com/ssafy/somelogin/controller/AuthController.java
@@ -1,26 +1,18 @@
 package com.ssafy.somelogin.controller;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 import com.ssafy.somelogin.dto.ResponseDto;
 import com.ssafy.somelogin.service.AuthService;
-import com.ssafy.somelogin.util.HttpUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.transaction.Transactional;
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
+
 
 @RestController
 @RequestMapping("/member")
@@ -28,10 +20,6 @@ import java.util.HashMap;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private String get_token_url;
-    private String client_id;
-    private String redirect_uri;
-    private final HttpUtil httpUtil;
     private final AuthService authService;
 
     @PostMapping("/kakao")
@@ -40,7 +28,7 @@ public class AuthController {
         String redirect_base = headers.get("redirect_base").toString();
         redirect_base = redirect_base.replace("[","");
         redirect_base = redirect_base.replace("]","");
-        log.debug("인가코드로 토큰 요청 POST: /member/kakao, code : {}, redirect : {}",authorization_code,redirect_base);
+        log.info("인가코드로 토큰 요청 POST: /member/kakao, code : {}, redirect : {}",authorization_code,redirect_base);
         ResponseDto responseDto = authService.getTokenAnduserInfo(authorization_code,redirect_base);
         return ResponseEntity.ok().body(responseDto);
     }
