e14e1e5f8b9c5a22ee401fc2b68537ec74a2db40
HyeonIn <gusdlsdlek@gmail.com>
2023-04-28 19:42:02 +0900

    [FE] fix : 알림 페이지 다크모드 및 디자인 수정

--- frontend/pages/notification.tsx
+++ frontend/pages/notification.tsx
@@ -1,6 +1,9 @@
-import { notiDummy } from "./api/notiDummyApi";
-import styles from "@/styles/notification.module.scss"
+// components
 import { InfoBar } from "@/components/common/Nav";
+// dummy data
+import { notiDummy } from "./api/notiDummyApi";
+// scss
+import styles from "@/styles/notification.module.scss"
 
 export default function Notification() {
     return(
@@ -11,19 +14,21 @@ export default function Notification() {
                     notiDummy.map((noti) => {
                         return(
                             <div key={noti.notificationId} className={noti.isRead ? `py-4 px-6` : `py-4 px-6 ${styles.on_read}`}>
+                                    
                                 <div className="flex justify-between">
                                     <div className="flex">
-                                        <p className="text-sm text-gray-400">{noti.albumName}</p>
+                                        <p className={noti.isRead ? "text-sm text-gray-300 dark:text-gray-600" : "text-sm text-gray-600 dark:text-gray-200"}>{noti.albumName}</p>
                                     </div>
-                                    <p className="text-sm text-gray-400">시간</p>
+                                    <p className={noti.isRead ? "text-sm text-gray-300 dark:text-gray-600" : "text-sm text-gray-600 dark:text-gray-200"}>시간</p>
                                 </div>
-                                <div className={noti.isRead ? "flex justify-between items-center mt-2 text-gray-400" : "mt-2 flex justify-between"}>
+                                <div className={noti.isRead ? "flex justify-between items-center mt-2 text-gray-300 dark:text-gray-600" : "mt-2 flex justify-between text-gray-600 dark:text-gray-200"}>
                                     <div className="flex items-center justify-center h-12">
                                         {noti.status === 1 && <p className="text-sm">{noti.nickname}님이 SNS에 올리기를 요청했습니다!</p>}
-                                        {noti.status === 2 && <p className="text-sm">{noti.nickname}님이 새로운 앨범에 초대했습니다!</p>}
+                                        {noti.status === 2 && <p className="text-sm">{noti.nickname}님이 {noti.albumName}에 초대했습니다!</p>}
                                     </div>
                                     {noti.status !== 2 && <img className="w-12 h-12 rounded-md" src={noti.notiImg} alt="" />}
                                 </div>
+
                             </div>
                         )
                     })
