f3e7996acab32964e4f5a066397ed572d8becfae
oth54477 <oth5447@naver.com>
2023-05-01 07:33:12 +0900

    [FE] fix : home api 연결

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,14 +1,29 @@
-import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
-import wrapper from "@/store";
+
+// React-Query
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import wrapper from "@/store";
+import "@/styles/globals.scss";
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // React-Query 옵션 설정
+    },
+  },
+});
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <Component {...pageProps} />
+      <QueryClientProvider client={client}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
