07ec6052e0cb4125b6bb9e23ccbc0d5a20acab42
seoyoon528 <seoyoon528@gmail.com>
2023-04-30 17:13:46 +0900

    [BE] add : friendboy album API Repository

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/repository/album/AlbumRepository.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/repository/album/AlbumRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AlbumRepository extends JpaRepository<Album,Long>, AlbumRepositoryCustom {
+
+    Album findAlbumByAlbumId(Long albumId);
 }
