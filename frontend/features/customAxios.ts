cba4ed0470e4c27913816e804f47132b551e9f6d
HyeonIn <gusdlsdlek@gmail.com>
2023-05-01 11:19:49 +0900

    [FE] add : axios 모듈화

---
+++ frontend/features/customAxios.ts
@@ -0,0 +1,26 @@
+// axios 사용
+import axios from "axios";
+
+// Redux 관련
+import { RootState } from "@/store";
+import { useSelector } from "react-redux";
+
+//redux에서 access token 받아오기
+const {userInfo} = useSelector((state : RootState) => state.auth);
+
+// 남사친 API 커스텀
+export const customBoyAxios = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_FRIEND_BOY_URL,
+    headers: {
+        "access_token" : userInfo.access_token
+    }
+});
+
+// 여사친 API 커스텀
+export const customGirlAxios = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_FRIEND_GIRL_URL,
+    headers: {
+        "access_token" : userInfo.access_token
+    }
+});
+
