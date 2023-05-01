177bf724ffa56cf5b3bdba9ccf5dc61f9d4007e0
차현경 <h30young@naver.com>
2023-05-01 10:27:31 +0900

    [BE] add : upload Transactional

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/controller/AlbumPhotoController.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/controller/AlbumPhotoController.java
@@ -13,6 +13,7 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -29,6 +30,7 @@ public class AlbumPhotoController {
     private final AlbumPhotoService albumPhotoService;
     private final ResourceLoader resourceLoader;
 
+    @Transactional
     @PostMapping("/upload")
     public ResponseEntity<ResponseDto> upload(@RequestHeader HttpHeaders headers,
                                               @RequestParam("multipartFile") List<MultipartFile> multipartFiles,
