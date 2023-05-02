01f59fdabaf0a75d0f2bf7cff1e4b6d3a9a19678
HyeonIn <gusdlsdlek@gmail.com>
2023-05-02 17:47:35 +0900

    [FE] fix : axios 모듈화

--- frontend/features/customAxios.ts
+++ frontend/features/customAxios.ts
@@ -1,32 +1,23 @@
 // axios 사용
 import axios from "axios";
 
-// Redux 관련
-import { RootState } from "@/store/configureStore";
-import { useSelector } from "react-redux";
-
 function useCustomAxios() {
-    let parseAccessToken;
-    const storageAccessToken = window.localStorage.getItem("access_token");
-
-    if (storageAccessToken) {
-        parseAccessToken = JSON.parse(storageAccessToken);
+    let parseToken = "";
+    if(typeof window !== 'undefined'){
+        parseToken = JSON.parse(window.localStorage.getItem("access_token") || '{}').access_token;
     }
-
     const customBoyAxios = axios.create({
         baseURL: process.env.NEXT_PUBLIC_FRIEND_BOY_URL,
         headers: {
-            "access_token" : parseAccessToken.access_token,
+            "access_token" : parseToken,
         }
     });
-
     const customGirlAxios = axios.create({
         baseURL: process.env.NEXT_PUBLIC_FRIEND_GIRL_URL,
         headers: {
-            "access_token" : parseAccessToken.access_token,
+            "access_token" : parseToken,
         }
     });
-    
     return {customBoyAxios, customGirlAxios}
 }
 
