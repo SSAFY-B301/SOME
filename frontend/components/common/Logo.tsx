2d6ba7a30bc05d52ed3957deda130647646a4c62
HyeonIn <gusdlsdlek@gmail.com>
2023-05-02 16:42:48 +0900

    [FE] add : 사진 상세보기 API 연결

--- frontend/components/common/Logo.tsx
+++ frontend/components/common/Logo.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
 import styles from "styles/home.module.scss";
 
 function Logo() {
-  return <h1 className={`text-3xl ${styles.logo}`}>SOME</h1>;
+  return (
+    <Link href="/photo/1">
+      <h1 className={`text-3xl ${styles.logo}`}>SOME</h1>
+    </Link>
+  )
 }
 
 export default Logo;
