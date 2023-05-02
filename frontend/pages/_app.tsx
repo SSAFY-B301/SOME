204f29f1a13abb446cf97a1fcab5ffbf1e7a75a6
HyeonIn <gusdlsdlek@gmail.com>
2023-05-02 14:49:22 +0900

    [FE] fix : pwa setting

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -19,6 +19,10 @@ const client = new QueryClient({
   },
 });
 
+// if('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('/service-worker.js');
+// };
+
 function App({ Component, ...rest }: AppProps) {
   const {store, props} = wrapper.useWrappedStore(rest);
   return (
