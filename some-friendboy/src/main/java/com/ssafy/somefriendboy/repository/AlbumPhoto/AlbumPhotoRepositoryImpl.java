99dce68ea87758f8c8ad3e7006426f8b7905b87d
차현경 <h30young@naver.com>
2023-05-03 16:29:01 +0900

    [BE] add : modify photo status

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/repository/AlbumPhoto/AlbumPhotoRepositoryImpl.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/repository/AlbumPhoto/AlbumPhotoRepositoryImpl.java
@@ -3,11 +3,13 @@ package com.ssafy.somefriendboy.repository.AlbumPhoto;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.somefriendboy.entity.AlbumPhoto;
+import com.ssafy.somefriendboy.entity.PhotoStatus;
 import com.ssafy.somefriendboy.util.MongoQueryUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 
 import java.util.List;
 
@@ -24,6 +26,7 @@ public class AlbumPhotoRepositoryImpl implements AlbumPhotoRepositoryCustom {
     public List<AlbumPhoto> findAlbumPhoto(Long albumId, Long categoryId, List<String> userId) {
         Query query = new Query();
         query.addCriteria(Criteria.where(MongoQueryUtil.parse(albumPhoto.albumId)).is(albumId));
+        query.addCriteria(Criteria.where(MongoQueryUtil.parse(albumPhoto.status)).is(PhotoStatus.NORMAL));
         if (categoryId != null) query.addCriteria(Criteria.where(MongoQueryUtil.parse(albumPhoto.categoryId)).in(categoryId));
         if (userId != null && userId.size() != 0) query.addCriteria(Criteria.where(MongoQueryUtil.parse(albumPhoto.userId)).in(userId));
         return mongoTemplate.find(query, AlbumPhoto.class);
@@ -33,6 +36,13 @@ public class AlbumPhotoRepositoryImpl implements AlbumPhotoRepositoryCustom {
     public List<Long> findCategoryName(List<String> categoryName) {
         return queryFactory.select(photoCategory.categoryId).from(photoCategory)
                 .where(photoCategory.categoryName.in(categoryName)).fetch();
+    }
+
+    @Override
+    public void modifyPhotoStatus(List<Long> photoId) {
+        Query query = new Query(Criteria.where(MongoQueryUtil.parse(albumPhoto.photoId)).in(photoId));
+        Update update = Update.update(MongoQueryUtil.parse(albumPhoto.status), PhotoStatus.DELETED);
+        mongoTemplate.updateMulti(query, update, AlbumPhoto.class);
     }
 
     private BooleanExpression categoryIdEq(Long categoryId) {
