351b2b7e1faf0e2f9ae5e422cfde82781eac57a4
차현경 <h30young@naver.com>
2023-05-03 16:29:01 +0900

    [BE] add : modify photo status

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/repository/AlbumPhoto/AlbumPhotoRepositoryCustom.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/repository/AlbumPhoto/AlbumPhotoRepositoryCustom.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface AlbumPhotoRepositoryCustom {
     List<AlbumPhoto> findAlbumPhoto(Long albumId, Long categoryId, List<String> userId);
     List<Long> findCategoryName(List<String> categoryName);
+    void modifyPhotoStatus(List<Long> photoId);
 }
