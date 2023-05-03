d1a8287d2b7be7d2a04b881c46efd132bf045968
HyeonIn <gusdlsdlek@gmail.com>
2023-05-03 20:53:28 +0900

    [FE] add : redux persist 설정 추가

--- frontend/store/reducer.ts
+++ frontend/store/reducer.ts
@@ -1,10 +1,18 @@
 import { combineReducers } from "redux";
 import { HYDRATE } from "next-redux-wrapper";
 import authSlice from "../features/authSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const combinedReducer = combineReducers({
   auth: authSlice,
 });
+
+const persistConfig = {
+  key : "root",
+  storage,
+  whitelist : ["auth"]
+}
 
 const rootReducer: typeof combinedReducer = (state, action) => {
   if (action.type === HYDRATE) {
@@ -17,4 +25,5 @@ const rootReducer: typeof combinedReducer = (state, action) => {
     return combinedReducer(state, action);
   }
 };
-export default rootReducer;
+
+export default persistReducer(persistConfig, rootReducer);
