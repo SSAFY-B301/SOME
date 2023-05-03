ea4090928aa7a6c1c76af73fbbc4f8c26744b26c
euntol <mokbee27@gmail.com>
2023-05-03 16:31:24 +0900

    [FE] design: 친구 초대 페이지 디자인 수정

--- frontend/components/album-starter/Album.tsx
+++ frontend/components/album-starter/Album.tsx
@@ -18,6 +18,7 @@ const Album = ({ album, selectAlbum, isActiveFriends }: Props): JSX.Element => {
   const select = () => {
     selectAlbum(album.members);
   };
+  console.log(`${album.album_name} ${album.id}`);
 
   return (
     <div
@@ -25,13 +26,18 @@ const Album = ({ album, selectAlbum, isActiveFriends }: Props): JSX.Element => {
       style={{ backgroundImage: "url(" + album.thumbnail_photo_url + ")" }}
       onClick={select}
     >
-      <div className="w-28 h-28 flex flex-col justify-end items-center">
-        <span className="text-2xl text-white text-center">
-          {album.album_name}
-        </span>
-        <span className="text-white text-center" style={{ fontSize: "8px" }}>
-          {album.album_created_date}
-        </span>
+      <div className="w-28 h-28 flex justify-center items-center">
+        <div className="w-24 h-24 flex flex-col justify-end items-center">
+          <span className="w-full text-base text-black text-center truncate">
+            {album.album_name}
+          </span>
+          <span
+            className="w-full text-black text-center "
+            style={{ fontSize: "8px" }}
+          >
+            {album.album_created_date.split("T", 1)}
+          </span>
+        </div>
       </div>
     </div>
   );
