16117e42896e773e2e38bc25a106f8396ef771c2
차현경 <h30young@naver.com>
2023-05-03 14:16:27 +0900

    [BE] fix : album list api annotation

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/dto/AlbumPhotoListOptDto.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/dto/AlbumPhotoListOptDto.java
@@ -1,10 +1,12 @@
 package com.ssafy.somefriendboy.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
 public class AlbumPhotoListOptDto {
 
     private Long albumId;
