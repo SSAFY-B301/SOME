2bf2ed836b061ec1c3c82157cc4f761d336ccbfb
seoyoon528 <seoyoon528@gmail.com>
2023-05-03 17:43:38 +0900

    [BE] add : /album/list/whole querydsl로 수정중, paging 처리중

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/repository/album/AlbumRepositoryCustom.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/repository/album/AlbumRepositoryCustom.java
@@ -2,6 +2,8 @@ package com.ssafy.somefriendboy.repository.album;
 
 import com.ssafy.somefriendboy.dto.AlbumWholeListDto;
 import com.ssafy.somefriendboy.entity.Album;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -10,4 +12,5 @@ public interface AlbumRepositoryCustom {
     long modifyAlbumName(Long albumId, String newName);
     long modifyAlbumThumbnail(Long albumId, Long photoId);
     long modifyAlbumRecentPhoto(Long albumId, Long photoId);
+    Page<AlbumWholeListDto> pageAlbumWholeListDto(List<Long> myAlbumIdList, Pageable pageable);
 }
