25e7ff4007dec81b942afb38593a63eb490b5ad0
euntol <mokbee27@gmail.com>
2023-05-04 20:55:13 +0900

    [FE] design: 친구 초대 페이지 상단부 수정

--- frontend/pages/album/create.tsx
+++ frontend/pages/album/create.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 
 /**
  * 앨범 생성 페이지
+ * 이슈
+ * 1. 상단 바 삭제 -> 다음 페이지로 넘어가는 버튼을 중앙 혹은 접근성이 좋은 위치에 배치
  */
 const AlbumCreate = (): JSX.Element => {
   /**
@@ -20,16 +22,7 @@ const AlbumCreate = (): JSX.Element => {
 
   const resultName = albumName ? albumName : defaultName.current;
 
-  return (
-    <div
-      className="flex flex-col items-center justify-between bg-pink-400 "
-      style={{ width: "100vw", height: "100vh" }}
-    >
-      <div className="flex items-center justify-center w-full h-16">
-        <div className="relative w-11/12 h-12">
-          <span className="absolute text-2xl text-white -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 whitespace-nowrap">
-            새로운 앨범 생성
-          </span>
+  /* <div className="relative w-11/12 h-12">
           <Link
             className="absolute right-0 text-lg font-thin text-white -translate-y-1/2 top-1/2"
             href={{
@@ -39,20 +32,34 @@ const AlbumCreate = (): JSX.Element => {
             as={`/invite`}
           >
             다음
-          </Link>
-        </div>
-      </div>
-      <div className="flex flex-col justify-between w-9/12 h-24">
-        <span className="text-3xl text-white">앨범 제목</span>
+          </Link> */
+
+  return (
+    <div
+      className="relative bg-pink-400 "
+      style={{ width: "100vw", height: "100vh" }}
+    >
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-between w-9/12 h-24">
+        <span className="text-2xl text-white">앨범 제목</span>
         <input
-          className="text-3xl text-white bg-pink-400 border-b-2 h-1/2 placeholder:italic placeholder:text-white placeholder:opacity-60"
+          className="text-2xl text-white bg-pink-400 border-b-2 h-1/2 placeholder:italic placeholder:text-white placeholder:opacity-60"
           placeholder={defaultName.current}
           value={albumName}
           onChange={onChange}
           maxLength={8}
         ></input>
+        <Link
+          className="text-lg font-thin text-white flex justify-end"
+          href={{
+            pathname: "/invite",
+            query: { albumId: "", albumName: resultName },
+          }}
+          as={`/invite`}
+        >
+          다음
+        </Link>
       </div>
-      <div className="box-border flex justify-center w-full h-20 pt-2">
+      <div className="absolute bottom-0 box-border flex justify-center w-full h-20 pt-2">
         <Link href={"/boy-home"}>
           <PlusIcon className="origin-center rotate-45" />
         </Link>
