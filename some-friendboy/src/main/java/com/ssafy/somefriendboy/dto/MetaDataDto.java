4a18472af02289574cdf9919992b9a291859ef2f
차현경 <h30young@naver.com>
2023-05-07 17:50:43 +0900

    [BE] add : MetaDataDto photoId

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/dto/MetaDataDto.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/dto/MetaDataDto.java
@@ -3,14 +3,12 @@ package com.ssafy.somefriendboy.dto;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
 @Builder
 public class MetaDataDto {
 
+    private Long photoId;
     private String originUrl;
     private String resizeUrl;
-    private List<Long> categoryId;
 
 }
