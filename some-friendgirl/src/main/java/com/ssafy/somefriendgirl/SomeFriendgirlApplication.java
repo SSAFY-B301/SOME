ffd2b160dfbf28332323db0f1883511546cdce4a
박서윤 <seoyoon528@gmail.com>
2023-05-08 23:59:09 +0900

    [BE] add : some-friendgirl project init

---
+++ some-friendgirl/src/main/java/com/ssafy/somefriendgirl/SomeFriendgirlApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.somefriendgirl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeFriendgirlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeFriendgirlApplication.class, args);
+	}
+
+}
