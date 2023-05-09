ecd0eecfcf8e7d3472628d02c7347b0a6f19d41f
차현경 <h30young@naver.com>
2023-05-09 11:24:14 +0900

    [BE] add : photo metadata dto

---
+++ some-friendgirl/src/main/java/com/ssafy/somefriendgirl/dto/GpsDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.somefriendgirl.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GpsDto {
+
+    private Double latitude;
+    private Double longitude;
+
+}
