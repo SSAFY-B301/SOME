80c54abe092d6b16c0defd6eeb5302139aa5611e
차현경 <h30young@naver.com>
2023-05-09 11:20:39 +0900

    [BE] add : AlbumPhoto entity

---
+++ some-friendgirl/src/main/java/com/ssafy/somefriendgirl/entity/AutoIncrementSequence.java
@@ -0,0 +1,16 @@
+package com.ssafy.somefriendgirl.entity;
+
+import lombok.Getter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Id;
+
+@Document
+@Getter
+public class AutoIncrementSequence {
+
+    @Id
+    private String id;
+    private Long seq;
+
+}
