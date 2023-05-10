162bebd2d256efe6c0773166f5fa2d68982f6593
seoyoon528 <seoyoon528@gmail.com>
2023-05-10 15:42:30 +0900

    [BE] add : /photo/like API repository 추가

--- some-friendgirl/src/main/java/com/ssafy/somefriendgirl/repository/photo/PhotoRepository.java
+++ some-friendgirl/src/main/java/com/ssafy/somefriendgirl/repository/photo/PhotoRepository.java
@@ -2,7 +2,9 @@ package com.ssafy.somefriendgirl.repository.photo;
 
 import com.ssafy.somefriendgirl.entity.AlbumPhoto;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PhotoRepository extends MongoRepository<AlbumPhoto, String>, PhotoRepositoryCustom {
     AlbumPhoto findByPhotoId(Long photoId);
 }
