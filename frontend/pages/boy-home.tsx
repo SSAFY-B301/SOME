81a539c2a8b7a94c639f79b29f21d031961bd27a
HyeonIn <gusdlsdlek@gmail.com>
2023-05-10 10:24:19 +0900

    [FE] add : 최근 업로드 기능 추가 및 구조 변경

--- frontend/pages/boy-home.tsx
+++ frontend/pages/boy-home.tsx
@@ -4,8 +4,9 @@ import React from "react";
 // 컴포넌트
 import NavBar from "@/components/common/Nav";
 import TabBar from "@/components/common/TabBar";
+import CurrentAlbum from "@/components/common/CurrentAlbum";
+import { FavoriteAlbum } from "@/components/common/Albums";
 import TotalAlbum from "@/components/common/TotalAlbum";
-import { CurrentAlbum, FavoriteAlbum } from "@/components/common/Albums";
 
 // CSS
 import styles from "@/styles/home.module.scss";
