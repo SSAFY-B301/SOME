0b1bda202e2b12b463721a4e9970f49237e83247
차현경 <h30young@naver.com>
2023-05-10 11:18:44 +0900

    [BE] add : find photo list

--- some-friendgirl/src/main/java/com/ssafy/somefriendgirl/repository/album/AlbumRepositoryCustom.java
+++ some-friendgirl/src/main/java/com/ssafy/somefriendgirl/repository/album/AlbumRepositoryCustom.java
@@ -1,4 +1,15 @@
 package com.ssafy.somefriendgirl.repository.album;
 
+import com.ssafy.somefriendgirl.dto.GpsRangeDto;
+import com.ssafy.somefriendgirl.entity.AlbumPhoto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface AlbumRepositoryCustom {
+    List<AlbumPhoto> findAllAlbumPhoto(GpsRangeDto gpsRangeDto);
+    List<AlbumPhoto> findTop4AlbumPhotoLikeCnt(GpsRangeDto gpsRangeDto);
+    Page<AlbumPhoto> findAlbumPhotoLikeCnt(GpsRangeDto gpsRangeDto, Pageable pageable);
+    Page<AlbumPhoto> findAlbumPhotoPhotoId(GpsRangeDto gpsRangeDto, Pageable pageable);
 }
--- some-friendgirl/src/main/java/com/ssafy/somefriendgirl/repository/album/AlbumRepositoryImpl.java
+++ some-friendgirl/src/main/java/com/ssafy/somefriendgirl/repository/album/AlbumRepositoryImpl.java
@@ -1,5 +1,62 @@
 package com.ssafy.somefriendgirl.repository.album;
 
+import com.ssafy.somefriendgirl.dto.GpsRangeDto;
+import com.ssafy.somefriendgirl.entity.AlbumPhoto;
+import com.ssafy.somefriendgirl.util.MongoQueryUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.support.PageableExecutionUtils;
+
+import java.util.List;
+
+import static com.ssafy.somefriendgirl.entity.QAlbumPhoto.albumPhoto;
+
+@RequiredArgsConstructor
 public class AlbumRepositoryImpl implements AlbumRepositoryCustom {
+
+    private final MongoTemplate mongoTemplate;
+
+    @Override
+    public List<AlbumPhoto> findAllAlbumPhoto(GpsRangeDto gpsRangeDto) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where(MongoQueryUtil.parse(albumPhoto.mapLatitude)).gte(gpsRangeDto.getStartLat()).lt(gpsRangeDto.getEndLat()));
+        query.addCriteria(Criteria.where(MongoQueryUtil.parse(albumPhoto.mapLongitude)).gte(gpsRangeDto.getStartLon()).lt(gpsRangeDto.getEndLon()));
+        return mongoTemplate.find(query, AlbumPhoto.class);
+    }
+
+    @Override
+    public List<AlbumPhoto> findTop4AlbumPhotoLikeCnt(GpsRangeDto gpsRangeDto) {
+        Query query = new Query().with(Sort.by(Sort.Direction.DESC, MongoQueryUtil.parse(albumPhoto.likeCnt))).limit(4);
+        query.addCriteria(Criteria.where(MongoQueryUtil.parse(albumPhoto.mapLatitude)).gte(gpsRangeDto.getStartLat()).lt(gpsRangeDto.getEndLat()));
+        query.addCriteria(Criteria.where(MongoQueryUtil.parse(albumPhoto.mapLongitude)).gte(gpsRangeDto.getStartLon()).lt(gpsRangeDto.getEndLon()));
+        return mongoTemplate.find(query, AlbumPhoto.class);
+    }
+
+    @Override
+    public Page<AlbumPhoto> findAlbumPhotoLikeCnt(GpsRangeDto gpsRangeDto, Pageable pageable) {
+        Query query = new Query().with(pageable).with(Sort.by(Sort.Direction.DESC, MongoQueryUtil.parse(albumPhoto.likeCnt)));
+        query.addCriteria(Criteria.where(MongoQueryUtil.parse(albumPhoto.mapLatitude)).gte(gpsRangeDto.getStartLat()).lt(gpsRangeDto.getEndLat()));
+        query.addCriteria(Criteria.where(MongoQueryUtil.parse(albumPhoto.mapLongitude)).gte(gpsRangeDto.getStartLon()).lt(gpsRangeDto.getEndLon()));
+        List<AlbumPhoto> albumPhotos = mongoTemplate.find(query, AlbumPhoto.class);
+
+        return PageableExecutionUtils.getPage(albumPhotos, pageable,
+                () -> mongoTemplate.count(Query.of(query).limit(-1).skip(-1), AlbumPhoto.class));
+    }
+
+    @Override
+    public Page<AlbumPhoto> findAlbumPhotoPhotoId(GpsRangeDto gpsRangeDto, Pageable pageable) {
+        Query query = new Query().with(pageable).with(Sort.by(Sort.Direction.DESC, MongoQueryUtil.parse(albumPhoto.photoId)));
+        query.addCriteria(Criteria.where(MongoQueryUtil.parse(albumPhoto.mapLatitude)).gte(gpsRangeDto.getStartLat()).lt(gpsRangeDto.getEndLat()));
+        query.addCriteria(Criteria.where(MongoQueryUtil.parse(albumPhoto.mapLongitude)).gte(gpsRangeDto.getStartLon()).lt(gpsRangeDto.getEndLon()));
+        List<AlbumPhoto> albumPhotos = mongoTemplate.find(query, AlbumPhoto.class);
+
+        return PageableExecutionUtils.getPage(albumPhotos, pageable,
+                () -> mongoTemplate.count(Query.of(query).limit(-1).skip(-1), AlbumPhoto.class));
+    }
 
 }
