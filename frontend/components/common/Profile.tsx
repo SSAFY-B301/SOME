5ec5283ce488a32807c8628ff2ae6fa1c5cda30d
HyeonIn <gusdlsdlek@gmail.com>
2023-05-11 09:14:21 +0900

    [FE] fix : user_img 에러 수정 및 auth API 설정 수정

--- frontend/components/common/Profile.tsx
+++ frontend/components/common/Profile.tsx
@@ -6,14 +6,14 @@ function Profile() {
   if (getUserInfo === undefined) {
     return (
       <div
-        className="w-6 h-6 rounded-full bg-gray-400"
+        className="w-6 h-6 bg-gray-400 rounded-full"
       ></div>
     )   
   }
   else{
     return (
       <div
-        className="w-6 h-6 rounded-full bg-center bg-cover"
+        className="w-6 h-6 bg-center bg-cover rounded-full"
         style={{ backgroundImage: "url(" + getUserInfo.user_img + ")" }}
       ></div>
     );
