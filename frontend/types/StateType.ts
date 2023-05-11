d02a135969291ca38a54fafb21fb4525c0e2932d
HyeonIn <gusdlsdlek@gmail.com>
2023-05-11 16:19:30 +0900

    [FE] add : 타입 설정 추가

--- frontend/types/StateType.ts
+++ frontend/types/StateType.ts
@@ -1,9 +1,15 @@
 import type { UserInfoType } from "@/types/UserType";
 
 export interface StateType {
-  auth: LoginStateType;
-  albumStatus: AlbumStatusType;
+  auth: LoginStateType,
+  location: locationStateType,
+  albumStatus: AlbumStatusType,
   photoUpload: photoUploadType;
+}
+
+export interface locationStateType{
+  lat : number,
+  lng : number,
 }
 
 export interface LoginStateType {
@@ -23,4 +29,13 @@ export interface photoUploadType {
   isPreview: boolean;
   previewLength: number;
   uploadLength: number;
+}
+
+export interface GirlListDetailStateType {
+  section : number,
+  latitude : number,
+  longitude : number,
+  page : number,
+  size : number,
+  order : string
 }
