d7f1f81aba97f7f170dce9b7b24627a013e71d26
HyeonIn <gusdlsdlek@gmail.com>
2023-05-11 16:13:23 +0900

    [FE] add : 여사친 목록 상세  리듀서 추가

---
+++ frontend/features/girlListDetailSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { GirlListDetailStateType } from "@/types/StateType";
+
+const initialState : GirlListDetailStateType= {
+    section : 5,
+    latitude : -1,
+    longitude : -1,
+    page : 0,
+    size : 12,
+    order : "like"
+};
+
+export const girlListDetailStateSlice = createSlice({
+  name: "girlListDetailState",
+  initialState,
+  reducers: {
+    girListDetailInit(state){
+      state.section = 5,
+      state.latitude = -1,
+      state.longitude = -1,
+      state.page = 0,
+      state.size = 12,
+      state.order = "like"
+    },
+    setGirlListDetailState(state, action) {
+      state.section = action.payload.section,
+      state.latitude = action.payload.latitude,
+      state.longitude = action.payload.longitude,
+      state.page = action.payload.page,
+      state.size = action.payload.size,
+      state.order = action.payload.order
+    },
+  },
+  extraReducers: (builder) => {},
+});
+
+export const { girListDetailInit, setGirlListDetailState } = girlListDetailStateSlice.actions;
+export default girlListDetailStateSlice.reducer;
