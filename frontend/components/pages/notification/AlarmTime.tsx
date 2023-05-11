92338ec69d6a6aa84ba2343517746bb1b88cb03f
HyeonIn <gusdlsdlek@gmail.com>
2023-05-11 16:16:04 +0900

    [FE] add : 여사친 상세 날짜 컴포넌트 추가

--- frontend/components/pages/notification/AlarmTime.tsx
+++ frontend/components/pages/notification/AlarmTime.tsx
@@ -5,6 +5,24 @@ interface AlarmTimeType {
     time : string
 }
 
+export function PhotoTime(params: AlarmTimeType) {
+    let startTime = new Date(params.time).getTime();
+    let nowTime = Date.now();
+    if(startTime - nowTime > -60000) {
+        return <Moment className="text-sm text-gray-400" format="방금 전">{startTime}</Moment>;
+    }
+    else if(startTime - nowTime < -86400000) {
+        return <Moment className="text-sm text-gray-400" format="MMM D일">{startTime}</Moment>;
+    }
+    else if(startTime - nowTime > -86400000) {
+        return <Moment className="text-sm text-gray-400" fromNow>{startTime}</Moment>;
+    }
+    else{
+        return <></>
+    }
+};
+
+
 export default function AlarmTime(params: AlarmTimeType) {
     let startTime = new Date(params.time).getTime();
     let nowTime = Date.now();
