b21b9c8a33b3e4ecc89c05ae5b4118c70223b4be
HyeonIn <gusdlsdlek@gmail.com>
2023-05-11 16:19:30 +0900

    [FE] add : 타입 설정 추가

--- frontend/types/GirlType.ts
+++ frontend/types/GirlType.ts
@@ -4,3 +4,66 @@ export interface GirlType{
     lat : number,
     lng : number,
 }
+
+export interface PhotoType{
+    mapLatitude : number, 
+    mapLongitude : number, 
+    photoId : number, 
+    s3Url : string,
+    uploadedDate : string
+    userId : string
+}
+
+export interface GirlResultType{
+    resultList : GirlReusltListType[],
+}
+
+export interface GirlReusltListType{
+    markGps : GirlListMarkGpsType,
+    totalPhotoCnt : number,
+    photoList : PhotoType[],
+}
+
+export interface GirlListMarkGpsType{
+    section : number,
+    latitude : number,
+    longitude : number,
+}
+export interface LocationType{
+    lat : number,
+    lng : number,
+  }
+export interface OrderType{
+    order : string,
+}
+
+export interface GirlRequestPartType {
+    formData: FormData;
+  }
+
+export interface GirlListDetailResultType{
+    page : PageType,
+    photoList : PhotoType[],
+    totalPhotoCnt : number,
+    totalUserCnt : number
+}
+export interface PageType{
+    is_first : true
+    is_last : boolean,
+    now_page : number,
+    total_page : number
+}
+
+export interface a {
+        photoId : number
+        uploadedDate : string,
+        s3Url : string,
+        mapLatitude : number,
+        mapLongitude : number,
+        userId : string,
+        userName : string,
+        userProfileImg : string,
+        likeCnt : number,
+        viewCnt : number,
+        userLikeStatus : boolean,
+    }
