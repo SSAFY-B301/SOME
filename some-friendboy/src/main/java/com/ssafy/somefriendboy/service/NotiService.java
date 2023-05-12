e308f8356d6f0b1156de96548e7c8cf529f50967
정상민 <jsangmin4175@gmail.com>
2023-05-12 11:27:50 +0900

    [BE] 마이페이지 API

--- some-friendboy/src/main/java/com/ssafy/somefriendboy/service/NotiService.java
+++ some-friendboy/src/main/java/com/ssafy/somefriendboy/service/NotiService.java
@@ -20,6 +20,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
@@ -31,6 +32,7 @@ import java.util.*;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional
 public class NotiService {
     private final FeedBackRepository feedBackRepository;
     private final HttpUtil httpUtil;
