b085652d1f0a918e7952d7579f0c0c86dd2648a7
HyeonIn <gusdlsdlek@gmail.com>
2023-05-12 15:30:03 +0900

    [FE] add : 마이페이지 API 연결

--- frontend/types/UserType.ts
+++ frontend/types/UserType.ts
@@ -3,3 +3,16 @@ export interface UserInfoType{
     user_img: string,
     user_name: string,
 }
+
+export interface MyPageDataType{
+    accept_album_cnt : number,
+    friendgirl_cnt : number,
+    invite_agree : boolean,
+    sns_agree : boolean,
+    upload_agree : boolean
+}
+
+export interface NotiOptionRequestType{
+    type : string,
+    is_agree : boolean
+}
