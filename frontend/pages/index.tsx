3a95b3bef612fb84fc29229a330d9773da017949
HyeonIn <gusdlsdlek@gmail.com>
2023-05-14 21:04:45 +0900

    [FE] fix : 알림 설정 중

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -21,19 +21,24 @@ export default function Home() {
     (state: StateType) => state.userAgent.userAgent
   );
   CheckDevice();
-
-  function NotificationPermission() {
-    Notification.requestPermission().then((result) => {
-      if (result === "granted") {
-          alert("알림 설정을 동의하셨습니다.");
-      }
-      else{
-        alert("알림 설정을 거부하셨습니다.");
-      }
-    });
-  }
+  
   useEffect(() => {
-    NotificationPermission();
+    if (!("Notification" in window)) {
+      // Check if the browser supports notifications
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+      // Check whether notification permissions have already been granted;
+      // if so, create a notification
+      const notification = new Notification("Hi there!");
+    } else if (Notification.permission !== "denied") {
+      // We need to ask the user for permission
+      Notification.requestPermission().then((permission) => {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          const notification = new Notification("Hi there!");
+        }
+      });
+    }
 
     let timeout;
     const isLogin =
