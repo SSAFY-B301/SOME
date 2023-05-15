a8b229ee9cbd4fd89a37a69e02d0cfac5a119de7
HyeonIn <gusdlsdlek@gmail.com>
2023-05-15 16:27:36 +0900

    [FE] fix : 알림 수정 끝

--- frontend/features/sse.ts
+++ frontend/features/sse.ts
@@ -1,39 +1,6 @@
 import { EventSourcePolyfill } from "event-source-polyfill";
 
-
-export const sendNotification = async (title : string, body : string) => {
-    const registration = await navigator.serviceWorker.getRegistration();
-
-    const showNotification = (title: string, body: string) => {
-        
-      const payload = {
-        body
-      };
-      if (registration !== undefined) {
-          if('showNotification' in registration) {
-            registration.showNotification(title, payload);
-          }
-          else {
-            new Notification(title, payload);
-          }
-      }
-    };
-    if(Notification.permission === 'granted') {
-        showNotification(title, body);
-    }
-    else {
-        if(Notification.permission !== 'denied') {
-            const permission = await Notification.requestPermission();
-            
-            if(permission === 'granted') {
-                showNotification(title, body);
-            }
-        }
-    }
-};
-
-
-export const sendNotification2 = async (title: string, content : string) => {
+export const sendNotification = async (title: string, content : string) => {
   
   if(Notification.permission === 'granted') {
     showNotification(title, content);
@@ -80,11 +47,12 @@ export function SseConnect(token : string) {
     };
     sse.onmessage = (event) => {
         const parseMsg = JSON.parse(event.data);
-        console.log(parseMsg);
         if (parseMsg.type === "SNS") {
+            console.log(parseMsg);
             sendNotification("SNS 공유 요청", parseMsg.content);
         }
         else if (parseMsg.type === "INVITE"){
+            console.log(parseMsg);
             sendNotification("새로운 앨범 초대", parseMsg.content);
         }
     }
