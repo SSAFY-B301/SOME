b3163f3efa7221a473a7e1c9e37ad6e3a3b29498
HyeonIn <gusdlsdlek@gmail.com>
2023-05-15 11:29:50 +0900

    [FE] fix : 알림 로직 수정

--- frontend/pages/oauth/redirect.tsx
+++ frontend/pages/oauth/redirect.tsx
@@ -1,15 +1,17 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 // axios
 import axios from "axios";
 
 
 // next Hooks
 import { useRouter } from "next/router";
+import { EventSourcePolyfill } from "event-source-polyfill";
 
 //로그인 완료 페이지니까 리덕스에 state 저장해주고 이동할 수 있도록
 export default function AuthRedirect() {
     // router 객체 생성
     const router = useRouter();
+    const [token, setToken] = useState("");
 
     function saveAccessToken(_access_token : string) {
         const accessTokenObj = { access_token : _access_token};
@@ -26,20 +28,90 @@ export default function AuthRedirect() {
                     },
                 });
             const responseAccessToken = userResult.data.data;
-
+            
             if (responseAccessToken !== null){
                 saveAccessToken(responseAccessToken);
-                router.push("/boy-home");
+                setToken(responseAccessToken);
+                
+            }
+        }
+        
+    }
+    async function showNotification(title : string, body : string) {
+        const registration = await navigator.serviceWorker.getRegistration();
+        
+        const payload = {
+            body
+        };
+
+        if (registration !== undefined) {
+            if('showNotification' in registration) {
+                registration.showNotification(title, payload);
+            }
+            else {
+                new Notification(title, payload);
             }
         }
     }
+    async function sendNoti(title : string, body : string) {
+        
+        if(Notification.permission === 'granted') {
+            showNotification(title, body);
+        }
+        else {
+            if(Notification.permission !== 'denied') {
+                const permission = await Notification.requestPermission();
+            
+                if(permission === 'granted') {
+                showNotification(title, body);
+                }
+            }
+        }
+    }
+    function SseConnect() {
+        const sse = new EventSourcePolyfill(`${process.env.NEXT_PUBLIC_SOME_NOTI_URL}/noti/subscribe`, {
+            headers: {
+                "access_token" : token,
+            },
+        });
+        sse.onopen = function () {
+            console.log("Notification Server Connected..");
+          };
+        sse.onerror = function (error) {
+            console.log("SSE Error Occured : ", error);
+            sse.close();
+        };
+        sse.onmessage = (event) => {
+            const parseMsg = JSON.parse(event.data);
+            console.log(parseMsg);
+            if (parseMsg.type === "SNS") {
+                sendNoti("SNS 공유 요청", parseMsg.data);
+            }
+            else if (parseMsg.type === "INVITE"){
+                sendNoti("새로운 앨범 초대", parseMsg.data);
+            }
+        }
+    };
+    async function notificationPermission() {
+        const permission = await Notification.requestPermission();
+    }
+    
+    useEffect(() => {
+      if (token !== "") {
+          SseConnect();
+          router.push("/boy-home");
+      }
+      return () => {
+      }
+    }, [token])
     
     // 렌더링 되면 authorization code URL에서 파싱
     useEffect(() => {
+      notificationPermission();  
       // authorization code URL에서 파싱해오기
       const params = new URL(document.location.toString()).searchParams;
       const paramsAuthCode = params.get("code");
-      
+
       // 인가 코드로 유저 데이터 받아오기
       getUserToken(paramsAuthCode);
       return () => {}
