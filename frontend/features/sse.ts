59153be4e0af5efbc53b8de0215b0be857c024bf
HyeonIn <gusdlsdlek@gmail.com>
2023-05-15 14:45:01 +0900

    [FE] fix : noti fix

--- frontend/features/sse.ts
+++ frontend/features/sse.ts
@@ -1,36 +1,38 @@
 import { EventSourcePolyfill } from "event-source-polyfill";
 
-async function showNotification(title : string, body : string) {
-    const registration = await navigator.serviceWorker.getRegistration();
-    
-    const payload = {
-        body
-    };
 
-    if (registration !== undefined) {
-        if('showNotification' in registration) {
+const sendNotification = async (title : string, body : string) => {
+    const registration = await navigator.serviceWorker.getRegistration();
+
+    const showNotification = (title: string, body: string) => {
+        
+      const payload = {
+        body
+      };
+      if (registration !== undefined) {
+          if('showNotification' in registration) {
             registration.showNotification(title, payload);
-        }
-        else {
+          }
+          else {
             new Notification(title, payload);
-        }
-    }
-}
-async function sendNoti(title : string, body : string) {
-    
+          }
+      }
+    };
     if(Notification.permission === 'granted') {
         showNotification(title, body);
     }
     else {
         if(Notification.permission !== 'denied') {
             const permission = await Notification.requestPermission();
-        
+            
             if(permission === 'granted') {
-            showNotification(title, body);
+                showNotification(title, body);
             }
         }
     }
-}
+};
+
+
 export function SseConnect(token : string) {
     const sse = new EventSourcePolyfill(`${process.env.NEXT_PUBLIC_SOME_NOTI_URL}/noti/subscribe`, {
         headers: {
@@ -48,10 +50,10 @@ export function SseConnect(token : string) {
         const parseMsg = JSON.parse(event.data);
         console.log(parseMsg);
         if (parseMsg.type === "SNS") {
-            sendNoti("SNS 공유 요청", parseMsg.content);
+            sendNotification("SNS 공유 요청", parseMsg.content);
         }
         else if (parseMsg.type === "INVITE"){
-            sendNoti("새로운 앨범 초대", parseMsg.content);
+            sendNotification("새로운 앨범 초대", parseMsg.content);
         }
     }
 };
