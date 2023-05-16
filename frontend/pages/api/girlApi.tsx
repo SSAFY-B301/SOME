29ff6ca2ee08a49ee28b7ddfe2dd8340c869b769
oth54477 <oth5447@naver.com>
2023-05-17 00:26:50 +0900

    [FE] fix : 여사친 페이지 처리

--- frontend/pages/api/girlApi.tsx
+++ frontend/pages/api/girlApi.tsx
@@ -1,56 +1,86 @@
 import useCustomAxios from "@/features/customAxios";
+import { setPage } from "@/features/girlListDetailSlice";
 import { RootState } from "@/store/configureStore";
-import { GirlResultType, GirlRequestPartType, GirlListDetailResultType, PhotoDetailType, GirlLikeRequestType } from "@/types/GirlType";
+import {
+  GirlResultType,
+  GirlRequestPartType,
+  GirlListDetailResultType,
+  PhotoDetailType,
+  GirlLikeRequestType,
+} from "@/types/GirlType";
+import { StateType } from "@/types/StateType";
 import { useRouter } from "next/router";
-import { useMutation, useQuery, useQueryClient } from "react-query";
-import { useSelector } from "react-redux";
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "react-query";
+import { useDispatch, useSelector } from "react-redux";
 
 const { customGirlAxios } = useCustomAxios();
 
 function getGirlList() {
-  const location = useSelector((state : RootState) => state.location)
-  const { data : queryData, isLoading : isGirlListLoading} = useQuery(["girlList", location], () => 
-  customGirlAxios.get(`/album/list?latitude=${location.lat}&longitude=${location.lng}`)
-  ,)
-  const resultData : GirlResultType = queryData?.data.data;
-  
-  return { resultData, isGirlListLoading}
+  const location = useSelector((state: RootState) => state.location);
+  const { data: queryData, isLoading: isGirlListLoading } = useQuery(
+    ["girlList", location],
+    () =>
+      customGirlAxios.get(
+        `/album/list?latitude=${location.lat}&longitude=${location.lng}`
+      )
+  );
+  const resultData: GirlResultType = queryData?.data.data;
+
+  return { resultData, isGirlListLoading };
 }
 
 function getGirlListDetail() {
-  const location = useSelector((state : RootState) => state.location)
-  const girlListDetailState = useSelector((state : RootState) => state.girlListDetailState);
-  const { data : queryData, isLoading : isGirlListLoading} = useQuery([`girlListDetail`, girlListDetailState], () => 
-    customGirlAxios.get(`/album/list/detail?latitude=${location.lat}&longitude=${location.lng}&section=${girlListDetailState.section}&sort=${girlListDetailState.order}&page=${girlListDetailState.page}&size=${girlListDetailState.size}`)
-  , {
-  })
-  const resultData : GirlListDetailResultType = queryData?.data.data;
-  return { resultData, isGirlListLoading}
+  const location = useSelector((state: RootState) => state.location);
+  const girlListDetailState = useSelector(
+    (state: RootState) => state.girlListDetailState
+  );
+  const { data: queryData, isLoading: isGirlListLoading } = useQuery(
+    [`girlListDetail`, girlListDetailState],
+    () =>
+      customGirlAxios.get(
+        `/album/list/detail?latitude=${location.lat}&longitude=${location.lng}&section=${girlListDetailState.section}&sort=${girlListDetailState.order}&page=${girlListDetailState.page}&size=${girlListDetailState.size}`
+      ),
+    {}
+  );
+  const resultData: GirlListDetailResultType = queryData?.data.data;
+  return { resultData, isGirlListLoading };
 }
 
 function getGirlPhotoDetail() {
   const router = useRouter();
   const photoId = router.query.photo_id;
 
-  const { data: queryData, isLoading, status : girlDetailStatus } = useQuery(["girlDetail", photoId], () =>
+  const {
+    data: queryData,
+    isLoading,
+    status: girlDetailStatus,
+  } = useQuery(["girlDetail", photoId], () =>
     customGirlAxios.get("/photo/detail/" + photoId)
   );
-  const resultData : PhotoDetailType = queryData?.data.data.albumPhotoDetail;
+  const resultData: PhotoDetailType = queryData?.data.data.albumPhotoDetail;
   return { resultData, isLoading, girlDetailStatus };
 }
 
-function useMutationGirl(){
-  const location = useSelector((state : RootState) => state.location)
+function useMutationGirl() {
+  const location = useSelector((state: RootState) => state.location);
   const queryClient = useQueryClient();
-  
+
   const headers = {
-    "Content-Type" : "multipart/form-data",
-  }
+    "Content-Type": "multipart/form-data",
+  };
 
   const { mutate: girlUploadMutation } = useMutation(
-    (requestData : GirlRequestPartType) => customGirlAxios.post("/album/upload?latitude=" + location.lat +"&longitude="+ location.lng, 
-    requestData.formData,
-    {headers}),
+    (requestData: GirlRequestPartType) =>
+      customGirlAxios.post(
+        "/album/upload?latitude=" + location.lat + "&longitude=" + location.lng,
+        requestData.formData,
+        { headers }
+      ),
     {
       onSuccess: (data) => {
         alert("사진이 등록되었습니다!");
@@ -61,18 +91,19 @@ function useMutationGirl(){
     }
   );
 
-  const { mutate : girlLikeMutation } = useMutation(
-    (requestData : GirlLikeRequestType) => customGirlAxios.put("/photo/like", requestData),
+  const { mutate: girlLikeMutation } = useMutation(
+    (requestData: GirlLikeRequestType) =>
+      customGirlAxios.put("/photo/like", requestData),
     {
-      onSuccess : () => {
+      onSuccess: () => {
         queryClient.invalidateQueries("girlList"); // queryKey 유효성 제거
         queryClient.invalidateQueries("girlListDetail"); // queryKey 유효성 제거
         queryClient.invalidateQueries("girlDetail"); // queryKey 유효성 제거
-      }
-    } 
-  )
+      },
+    }
+  );
 
-  return { girlUploadMutation, girlLikeMutation }
+  return { girlUploadMutation, girlLikeMutation };
 }
 
 export { getGirlList, getGirlListDetail, getGirlPhotoDetail, useMutationGirl };
