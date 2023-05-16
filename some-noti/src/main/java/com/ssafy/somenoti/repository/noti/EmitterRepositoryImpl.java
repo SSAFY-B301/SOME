112e3ec7132f604741e921ae8cf8df42eee456ee
정상민 <jsangmin4175@gmail.com>
2023-05-16 15:39:25 +0900

    [BE] modify: log 정리

--- some-noti/src/main/java/com/ssafy/somenoti/repository/noti/EmitterRepositoryImpl.java
+++ some-noti/src/main/java/com/ssafy/somenoti/repository/noti/EmitterRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.ssafy.somenoti.repository.noti;
 
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -8,6 +9,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @Repository
+@NoArgsConstructor
 public class EmitterRepositoryImpl implements EmitterRepository{
     private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
     private final Map<String, Object> eventCache = new ConcurrentHashMap<>();
