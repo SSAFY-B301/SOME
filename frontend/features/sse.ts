2fc4ad1f1c8c6629ee0103625e58d4b95826f908
HyeonIn <gusdlsdlek@gmail.com>
2023-05-16 13:38:11 +0900

    [FE] fix : 알림 수정

--- frontend/features/sse.ts
+++ frontend/features/sse.ts
@@ -1,70 +1,19 @@
 import { EventSourcePolyfill } from "event-source-polyfill";
 
 
-export const sendNotification = async (title : string, body : string) => {
-    const registration = await navigator.serviceWorker.getRegistration();
 
-    const showNotification = (title: string, body: string) => {
-        
-      const payload = {
-        body
-      };
-      if (registration !== undefined) {
-          if('showNotification' in registration) {
-            registration.showNotification(title, payload);
-          }
-          else {
-            new Notification(title, payload);
-          }
-      }
-    };
-    if(Notification.permission === 'granted') {
-        showNotification(title, body);
-    }
-    else {
-        if(Notification.permission !== 'denied') {
-            const permission = await Notification.requestPermission();
-            
-            if(permission === 'granted') {
-                showNotification(title, body);
-            }
-        }
-    }
-};
-
-
-export const sendNotification2 = async (title: string, content : string) => {
+export const sendNotification = async (title: string, content : string) => {
   
-  if(Notification.permission === 'granted') {
-    showNotification(title, content);
-  }
-  else {
-    if(Notification.permission !== 'denied') {
-      const permission = await Notification.requestPermission();
-  
-      if(permission === 'granted') {
-        showNotification(title, content);
-      }
-    }
-  }
-};
-  
-const showNotification = async (title: string, body : string) => {
   const registration = await navigator.serviceWorker.getRegistration();
-  
-  const payload = {
-    body
-  };
   if (registration !== undefined) {
-      if('showNotification' in registration) {
-        registration.showNotification(title, payload);
-      }
-      else {
-        new Notification(title, payload);
-      }
+    if('showNotification' in registration) {
+      registration.showNotification(title, {
+        body : content
+      });
+    }
   }
 };
-
+  
 export function SseConnect(token : string) {
     const sse = new EventSourcePolyfill(`${process.env.NEXT_PUBLIC_SOME_NOTI_URL}/noti/subscribe`, {
         headers: {
@@ -82,11 +31,11 @@ export function SseConnect(token : string) {
         const parseMsg = JSON.parse(event.data);
         if (parseMsg.type === "SNS") {
             console.log(parseMsg);
-            sendNotification2("SNS 공유 요청", parseMsg.content);
+            sendNotification("SNS 공유 요청", parseMsg.content);
         }
         else if (parseMsg.type === "INVITE"){
             console.log(parseMsg);
-            sendNotification2("새로운 앨범 초대", parseMsg.content);
+            sendNotification("새로운 앨범 초대", parseMsg.content);
         }
     }
 };
--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -3,3 +3,12 @@ self.addEventListener("install", function (event) {});
 this.addEventListener("fetch", function (event) {
   // it can be empty if you just want to get rid of that error
 });
+
+self.addEventListener("message", (event) => {
+  const parseData = JSON.parse(event.data);
+  event.waitUntil(
+    self.registration.showNotification(parseData.type, {
+      body: parseData.content,
+    })
+  );
+});
