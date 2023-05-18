07e3a26f086736ff2ab5aaf142a7261db87e4fd7
oth54477 <oth5447@naver.com>
2023-05-18 17:55:42 +0900

    [FE] fix : SNS 공유 크기 변경

--- frontend/components/photo-detail/VoteModal.tsx
+++ frontend/components/photo-detail/VoteModal.tsx
@@ -141,20 +141,20 @@ const VoteModal = ({ clickVote, requestSns, photoId }: Props): JSX.Element => {
       className="fixed left-0 top-0 w-screen h-screen z-20 bg-black/50 flex justify-center items-center"
       onClick={() => clickVote(photoId)}
     >
-      <div className="w-11/12 h-full flex flex-col justify-end items-center">
-        <div className="w-full h-36 bg-white dark:bg-dark-block rounded-xl flex flex-col justify-center items-center">
-          <div className="w-full h-1/2 border-b-2 flex justify-center items-center text-gray-400 dark:text-white">
+      <div className="w-11/12 h-24 opacity-90 flex flex-col justify-end items-center">
+        <div className="w-full h-3/6 opacity-90 bg-white dark:bg-dark-block rounded-xl flex flex-col justify-center items-center">
+          <div className="w-full h-4/6 border-b-2 flex justify-center items-center text-gray-400 dark:text-white text-sm">
             공유 요청 알림이 보내집니다.
           </div>
           <button
-            className="w-full h-1/2 flex justify-center items-center text-2xl font-bold text-blue-500"
+            className="w-full h-1/2 flex justify-center items-center text-lg font-bold text-blue-500"
             onClick={requestSns}
           >
             알림 보내기
           </button>
         </div>
         <button
-          className="w-full h-16 bg-white dark:bg-dark-block rounded-xl flex justify-center items-center box-border mt-2 mb-8 text-2xl font-bold"
+          className="w-full h-14 opacity-90 bg-white dark:bg-dark-block rounded-xl flex justify-center items-center box-border mt-2 mb-8 text-lg font-bold"
           onClick={() => clickVote(photoId)}
         >
           취소
